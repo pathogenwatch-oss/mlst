e3428eb1b4b003591d9639a3818725f392446360
Ben Taylor <ben.taylor@sanger.ac.uk>
2017-07-17 14:35:42 +0100

    Use http_proxy if it exists during the build

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+PROXY_VARIABLES=$(env | grep -iE '^https?_proxy=')
+if [ -z "$PROXY_VARIABLES" ]; then
+  PROXY_VARIABLES=''
+else
+  PROXY_VARIABLES=$(echo "$PROXY_VARIABLES" | sed 's/^/--build-arg /' | tr '\n' ' ')
+fi
+
 docker build \
-  -t registry.gitlab.com/cgps/wgsa/tasks/mlst:v3 \
+  $PROXY_VARIABLES \
+  -t registry.gitlab.com/cgps/wgsa/tasks/mlst:v4 \
   .
