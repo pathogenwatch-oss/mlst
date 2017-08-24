af63a644033573e3348f6ffa929a0e5a60b62a48
KAD <kad0xf@gmail.com>
2017-08-24 12:31:18 +0100

    Correct image name

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t registry.gitlab.com/cgps/wgsa/tasks/mlst:v4 .
-docker push registry.gitlab.com/cgps/wgsa/tasks/mlst:v4
+docker build -t registry.gitlab.com/cgps/wgsa-tasks/mlst:v4 .
+docker push registry.gitlab.com/cgps/wgsa-tasks/mlst:v4
